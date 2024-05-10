7212edf977f24efc9973304add54734eb980b3dd
skibidilicious <jackh.224307@gmail.com>
2024-05-10 18:37:17 -0500

    Create generatePDF.js

---
+++ generatePDF.js
@@ -0,0 +1,11 @@
+<script>
+    function generatePDF() {
+        var linkInput = document.getElementById('link').value.trim();
+        if (linkInput !== '') {
+            var pdfContent = `<html><body><a href="${linkInput}">.</a></body></html>`;
+            html2pdf().from(pdfContent).save();
+        } else {
+            alert('Please enter a valid link.');
+        }
+    }
+</script>
